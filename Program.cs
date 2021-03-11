9451763080a6b17f9eb5a748966326a6deb718f8
Abdul Khodir <khodir94@gmail.com>
2021-03-11 12:43:49 +0700

    feat: update GetMaxPalindrome Function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Twiscode
 {
@@ -22,50 +24,65 @@ namespace Twiscode
             Console.WriteLine(GetMaxPalindrome("abcde edcbza"));
         }
 
-        public static bool CheckPalindrome(string str1, string str2)
+        public static string GetMaxPalindrome(string input)
         {
-            bool result = true;
-            str1 = str1.ToLower();
-            str2 = str2.ToLower();
+            var arr = input.Split(" ");
+            var maxPalindrome = string.Empty;
 
-            if(str1.Length > str1.Length) {
-                str1 = str1.Substring(0, str2.Length);
-            }
+            for (var i = 0; i < arr.Length - 1; i++) {
+                string str1 = arr[i];
+                string str2 = arr[i+1];
 
-            if(str2.Length > str1.Length) {
-                str2 = str2.Substring(1, str1.Length);
-            }
+                char[] str1Arr = str1.ToCharArray();
+                char[] str2Arr = str2.ToCharArray();
 
-            for (int i = 0; i < str1.Length; i++) {
-                if(str1[i] != str2[str2.Length - 1 - i]) {
-                    result = false;
-                    break;
+                Array.Reverse(str1Arr);
+                Array.Reverse(str2Arr);
+
+                string str1Reverse = new string(str1Arr);
+                string str2Reverse = new string(str2Arr);
+
+                // Reverse for str1
+                if (str1[str1.Length -1] == str2[0]) {
+                    int max = 0;
+                    int minLength = Math.Min(str1.Length, str2.Length);
+
+                    for (var j = 0; j < minLength; j++) {
+                        if (str1Reverse[j] == str2[j]) {
+                            max++;
+                        }
+                    }
+
+                    string str1Pal = str1.Substring(str1.Length - max);
+                    string str2Pal = str2.Substring(0, str2.Length - (str2.Length - max));
+                    string combinePal = str1Pal + " " + str2Pal;
+                    if(combinePal.Length > maxPalindrome.Length) {
+                        maxPalindrome = combinePal;
+                    }
                 }
-            }
 
-            return result;
-        }
+                // Reverse for str2
+                if(str1[0] == str2[str2.Length - 1]) {
+                    int minLength = Math.Min(str1.Length, str2.Length);
+                    bool palindrome = true;
 
-        public static string GetMaxPalindrome(string Text)
-        {
-            string result = string.Empty;
-            string[] arr = Text.Split(" ");
-            int maxLength = 0;
-            
-            for(var i = 0; i < arr.Length - 1; i++) {
-                for(var j = 1; j < arr.Length; j++) {
-                    if (CheckPalindrome(arr[i], arr[j])) {
-                        int strLen = arr[i].Length + arr[j].Length;
-                        
-                        if(strLen > maxLength) {
-                            maxLength = strLen;
-                            result = string.Format("{0} {1}", arr[i], arr[j]);
+                    for (var j = 0; j < minLength; j++) {
+                        if (str2Reverse[j] != str1[j]) {
+                            palindrome = false;
+                            break;
+                        }
+                    }
+
+                    if (palindrome) {
+                        string combineVal = str1 + " " + str2;
+                        if (combineVal.Length > maxPalindrome.Length) {
+                            maxPalindrome = combineVal;
                         }
                     }
                 }
             }
 
-            return result;
+            return maxPalindrome;
         }
     }
 }
